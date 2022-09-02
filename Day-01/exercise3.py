6c4f44219e742f6e7d2d7409d0dc6ce731054131
Anavila <anavila.developer@gmail.com>
2022-09-01 23:24:12 -0500

    New files: 	Day-01/exercise1.py (Completed) 	Day-01/exercise2.py (Completed) 	Day-01/exercise3.py (Completed) 	Day-01/exercise4.py (Completed)

---
+++ Day-01/exercise3.py
@@ -0,0 +1,11 @@
+# Instructions
+# ===================================================================
+
+# Write a program that prints the number of characters
+# in a user's name.
+
+# ===================================================================
+
+a = input('What is your name?')
+
+print(len(a))
---
+++ Day-01/exercise4.py
@@ -0,0 +1,25 @@
+# Instructions
+# ===================================================================
+
+# Write a program that switches the values
+# stored in the variables a and b.
+
+# ===================================================================
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
