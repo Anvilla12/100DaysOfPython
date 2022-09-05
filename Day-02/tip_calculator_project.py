4bd503ebc0b3795a7f1dfb7afe39423203336f60
Anavila <anavila.developer@gmail.com>
2022-09-05 11:40:38 -0500

    Modified:   Day-02/tip_calculator_project.py

--- Day-02/tip_calculator_project.py
+++ Day-02/tip_calculator_project.py
@@ -4,8 +4,14 @@ print('''
 ========================================================================
 ''')
 
-total = input("🧾 What's the total bill? ")
-percentage = input("What's the percentage you would like to give?\n(5, 10, 12, 15)\n")
-people = input('How many people to split the bill? ')
+total = float(input("🧾 What's the total bill? $"))
+percentage = int(input("What's the percentage you would like to give?\n(5, 10, 12, 15)\n"))
+people = int(input('How many people to split the bill? '))
 
-tip = total
+tip = (total * (percentage / 100))
+
+pay_for_person = round((total + tip) / people, 2)
+
+pay_format = "{:.2f}".format(pay_for_person)
+
+print(f'Each person should pay ${pay_format}')
